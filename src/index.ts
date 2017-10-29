24ef6e7742c936b73ecf916aeb56ebd69c6a486c
Alex Seidler <alex.seidler@futurice.com>
2017-10-29 20:19:41 +0200

    Collect two-node references to a matrix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import {promisify} from 'util';
 import * as postcss from 'postcss';
-import {omit} from 'lodash';
+import {omit, flatMap, map, sortBy} from 'lodash';
 
 const syntax = require('postcss-scss');
 const multiGlob = require('multi-glob');
@@ -15,6 +15,8 @@ function usage() {
   `;
 }
 
+//
+// AST helpers
 interface ASTWalker {
   (node: postcss.Node): void;
 }
@@ -50,6 +52,56 @@ function filterAST(fn: ASTFilter, node: postcss.Node) {
   }
 }
 
+//
+// Analyzation matrix
+function isDeclaration(node: postcss.Node): node is postcss.Declaration {
+  return node.type === 'decl';
+}
+
+interface MatrixRow {
+  [key: string]: number;
+}
+
+interface Matrix {
+  [key: string]: MatrixRow;
+}
+
+function createMatrix(rules: postcss.Rule[]): Matrix {
+  const matrix: Matrix = {};
+
+  rules.forEach(rule => {
+    if (!rule.nodes)
+      return;
+
+    const decls = rule.nodes.filter(isDeclaration) as postcss.Declaration[];
+    for(let i = 0, l = decls.length; i < l; i++) {
+      for(let j = i + 1, l = decls.length; j < l; j++) {
+        // NOTE: Do I really need to normalize the matrix? Doesn't
+        // absolute count actually be valuable information?
+        const decl1 = decls[i];
+        const decl2 = decls[j];
+        if(!matrix[decl1.prop])
+          matrix[decl1.prop] = {};
+        matrix[decl1.prop][decl2.prop] = (matrix[decl1.prop][decl2.prop] || 0) + 1;
+      }
+    }
+  });
+
+  return matrix;
+}
+
+function sortMatrix(matrix: Matrix) {
+  const sortData = flatMap(matrix, (row, prop1) => map(row, (value, prop2) => [value, prop1, prop2]));
+  return sortBy(sortData, ([value]) => -value);
+}
+
+function analyzeMatrix(matrix: Matrix) {
+  const sorted = sortMatrix(matrix);
+  return sorted;
+}
+
+//
+// Parse AST and collect Rules
 function isRule(node: postcss.Node): node is postcss.Rule {
   return node.type === 'rule';
 }
@@ -57,10 +109,16 @@ function isRule(node: postcss.Node): node is postcss.Rule {
 async function csslusterfuckFile(filename: string): Promise<any> {
   const data = await readFile(filename);
   const result = await postcss([]).process(data, {syntax});
-  const nodes = filterAST(isRule, result.root as postcss.Node) as postcss.Rule[];
-  console.log('Rules', ...nodes);
+  const rules = filterAST(isRule, result.root as postcss.Node) as postcss.Rule[];
+  // console.log('Rules', ...rules);
+  const matrix = createMatrix(rules)
+  // console.log('Matrix', matrix);
+  const analysis = analyzeMatrix(matrix);
+  console.log('Analysis', analysis);
 }
 
+//
+// Entry point
 function csslusterfuck(filenames: string[]): Promise<postcss.Root[]> {
   const promises = filenames.map(csslusterfuckFile);
   return Promise.all(promises);
