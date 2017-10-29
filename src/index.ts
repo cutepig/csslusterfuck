86859e979de3cf53ac78d2a854ebf091c94cb501
Alex Seidler <alex.seidler@futurice.com>
2017-10-29 19:47:36 +0200

    Filter the rules from AST

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import {promisify} from 'util';
 import * as postcss from 'postcss';
+import {omit} from 'lodash';
 
 const syntax = require('postcss-scss');
 const multiGlob = require('multi-glob');
@@ -9,24 +10,62 @@ const readFile = promisify(fs.readFile);
 const glob = promisify(multiGlob.glob);
 
 function usage() {
-  return `Clusterfuck
-  Usage: clusterfuck /path/to/style.scss
+  return `CSSlusterfuck
+  Usage: csslusterfuck /path/to/style.scss
   `;
 }
 
-async function clusterfuckSingleFile(filename: string): Promise<postcss.Root> {
-  const data = await readFile(filename);
-  const result = await postcss([]).process(data, {syntax});
-  const root = result.root as postcss.Root;
-  return root;
+interface ASTWalker {
+  (node: postcss.Node): void;
 }
 
-async function clusterfuck(filenames: string[]): Promise<postcss.Root[]> {
-  const promises = filenames.map(clusterfuckSingleFile);
+function walkAST(fn: ASTWalker, node: postcss.Node): void {
+  fn(node);
+  switch (node.type) {
+    case 'root':
+    case 'rule':
+    case 'atrule':
+      if (node.nodes)
+        node.nodes.forEach(child => walkAST(fn, child));
+      // Intentional fall-through
+    case 'decl':
+    case 'comment':
+  }
+}
+
+interface ASTFilter extends ASTWalker {
+  (node: postcss.Node): boolean;
+}
+
+function filterAST(fn: ASTFilter, node: postcss.Node) {
+  const nodes: postcss.Node[] = [];
+
+  walkAST(_filterAST, node);
+
+  return nodes;
+
+  function _filterAST(node: postcss.Node) {
+    if (fn(node))
+      nodes.push(node);
+  }
+}
+
+function isRule(node: postcss.Node): node is postcss.Rule {
+  return node.type === 'rule';
+}
+
+async function csslusterfuckFile(filename: string): Promise<any> {
+  const data = await readFile(filename);
+  const result = await postcss([]).process(data, {syntax});
+  const nodes = filterAST(isRule, result.root as postcss.Node) as postcss.Rule[];
+  console.log('Rules', ...nodes);
+}
+
+function csslusterfuck(filenames: string[]): Promise<postcss.Root[]> {
+  const promises = filenames.map(csslusterfuckFile);
   return Promise.all(promises);
 }
 
 glob(process.argv[2])
-  .then(clusterfuck)
-  .then((ast: postcss.Root[]) => console.log(ast))
+  .then(csslusterfuck)
   .catch((err: Error) => console.log(err.message));
