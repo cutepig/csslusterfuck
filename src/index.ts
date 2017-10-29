e6278e7af121424692245514b5486c34e45e7f96
Alex Seidler <alex.seidler@futurice.com>
2017-10-29 20:55:20 +0200

    Sort and print analyzed rules against the matrix

--- src/index.ts
+++ src/index.ts
@@ -66,7 +66,7 @@ interface Matrix {
   [key: string]: MatrixRow;
 }
 
-function createMatrix(rules: postcss.Rule[]): Matrix {
+function _createMatrix(rules: postcss.Rule[]): Matrix {
   const matrix: Matrix = {};
 
   rules.forEach(rule => {
@@ -80,9 +80,14 @@ function createMatrix(rules: postcss.Rule[]): Matrix {
         // absolute count actually be valuable information?
         const decl1 = decls[i];
         const decl2 = decls[j];
+
         if(!matrix[decl1.prop])
           matrix[decl1.prop] = {};
         matrix[decl1.prop][decl2.prop] = (matrix[decl1.prop][decl2.prop] || 0) + 1;
+
+        if(!matrix[decl2.prop])
+          matrix[decl2.prop] = {};
+        matrix[decl2.prop][decl1.prop] = (matrix[decl2.prop][decl1.prop] || 0) + 1;
       }
     }
   });
@@ -90,13 +95,55 @@ function createMatrix(rules: postcss.Rule[]): Matrix {
   return matrix;
 }
 
-function sortMatrix(matrix: Matrix) {
+function _sortMatrix(matrix: Matrix) {
   const sortData = flatMap(matrix, (row, prop1) => map(row, (value, prop2) => [value, prop1, prop2]));
   return sortBy(sortData, ([value]) => -value);
 }
 
-function analyzeMatrix(matrix: Matrix) {
-  const sorted = sortMatrix(matrix);
+type VectorWeight = {
+  vector: number[];
+  rule: postcss.Rule;
+  weight: number;
+}
+
+function analyzeRules(rules: postcss.Rule[]): VectorWeight[] {
+  const matrix = _createMatrix(rules);
+  // Not sure if I even need to sort these..
+  // const sorted = _sortMatrix(matrix);
+  // ...Just go over the rules again, check it's values against the matrix
+  // Collect decl vectors from rule, for instance [decl1, decl2, decl3]
+  // Create index vectors from different permuations to get [[prop1 prop2] [prop1 prop3] [prop2 prop3]]
+  // Map those into a vector of [value1 value2 value3] and compute vector length
+  const vectors = rules.map(rule => {
+    if (!rule.nodes)
+      return;
+
+    const decls = rule.nodes.filter(isDeclaration) as postcss.Declaration[];
+    if (decls.length < 2)
+      return;
+
+    const vector = [];
+    for(let i = 0, l = decls.length; i < l; i++) {
+      for(let j = i + 1, l = decls.length; j < l; j++) {
+        const decl1 = decls[i];
+        const decl2 = decls[j];
+        vector.push(matrix[decl1.prop][decl2.prop]);
+      }
+    }
+
+    return {vector, rule};
+  })
+
+  // We should connect these to the relevant css rule
+  const weights = vectors
+    .filter(rules => !!rules)
+    // Normalize vectors
+    .map(vector => ({
+      ...vector,
+      weight: Math.sqrt(vector!.vector.reduce((a, b) => a + b * b, 0))
+    } as VectorWeight));
+
+  const sorted = sortBy(weights, (vector: VectorWeight) => -vector.weight);
   return sorted;
 }
 
@@ -111,10 +158,13 @@ async function csslusterfuckFile(filename: string): Promise<any> {
   const result = await postcss([]).process(data, {syntax});
   const rules = filterAST(isRule, result.root as postcss.Node) as postcss.Rule[];
   // console.log('Rules', ...rules);
-  const matrix = createMatrix(rules)
-  // console.log('Matrix', matrix);
-  const analysis = analyzeMatrix(matrix);
-  console.log('Analysis', analysis);
+  const analysis = analyzeRules(rules);
+  analysis.forEach((vector) => {
+    const rule: postcss.Rule = vector.rule;
+    console.log(`[Check] ${filename}#\
+    ${rule.source.start!.line}:${rule.source.start!.column}-${rule.source.end!.line}:${rule.source.end!.column} \
+    '${rule.selector}'`);
+  })
 }
 
 //
@@ -126,4 +176,4 @@ function csslusterfuck(filenames: string[]): Promise<postcss.Root[]> {
 
 glob(process.argv[2])
   .then(csslusterfuck)
-  .catch((err: Error) => console.log(err.message));
+  .catch((err: Error) => console.log(err.message, err));
