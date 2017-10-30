919b8af8082bd0cd993704fafb4ded60c4117f89
Alex Seidler <sopo.porsas@gmail.com>
2017-10-31 00:19:18 +0200

    Add CLI options handling (#1)

--- src/csslusterfuck.ts
+++ src/csslusterfuck.ts
@@ -1,1 +1,72 @@
-import './index';
+/* tslint:disable no-console */
+import * as args from 'args'
+import * as fs from 'fs'
+import * as multiGlob from 'multi-glob'
+import * as postcss from 'postcss'
+import {promisify} from 'util'
+import csslusterfuck from './index'
+
+const readFile = promisify(fs.readFile)
+const glob = promisify(multiGlob.glob)
+
+args
+  .option(
+    'mode',
+    `I've only bothered to implement 'dumb' mode. Try that`,
+    'dumb'
+  )
+  .option('unnormalized', 'Analyzation values will not be normalized')
+  .option(['n', 'count'], 'Number of rules to show')
+
+function getLineNumber(rule: postcss.Rule) {
+  // `rule.source.end` is also available to denote endpoint
+  return `${rule.source.start!.line}:${rule.source.start!.column}`
+}
+
+function reportSuccess(analysis, options) {
+  if (analysis.length > 0)
+    console.log('I might have found some obnoxious CSS rules:\n')
+
+  const count = +options.count
+  if (isNaN(options.count) || count < 1)
+    throw new Error('You stupid! Count has to be a number from 1 to Infinity!')
+
+  analysis.slice(0, +options.count).forEach(vector => {
+    const rule: postcss.Rule = vector.rule
+    console.log(`${vector.filename}#${getLineNumber(rule)} (${vector.weight})`)
+    // TODO: Eliminate rule children
+    console.log(rule.toString())
+  })
+}
+
+function reportError(error) {
+  console.log(error)
+}
+
+async function cli(filenames, options) {
+  console.log('cli', filenames, options)
+  const _options = {
+    ...options,
+    count: options.count,
+    n: options.n,
+    filenames,
+  }
+
+  const sources = await Promise.all<string>(
+    filenames.map(filename => readFile(filename))
+  )
+  const result = csslusterfuck(sources, _options)
+  return result
+}
+
+const argv = args.parse(process.argv, {name: 'csslusterfuck'})
+
+if (!args.raw._.length) {
+  args.showHelp()
+  process.exit()
+}
+
+glob(args.raw._)
+  .then(filenames => cli(filenames, argv))
+  .then(data => reportSuccess(data, argv))
+  .catch(reportError)
--- src/index.ts
+++ src/index.ts
@@ -1,196 +1,38 @@
-import * as fs from 'fs';
-import {promisify} from 'util';
-import * as postcss from 'postcss';
-import {omit, flatMap, map, sortBy} from 'lodash';
+import {defaults} from 'lodash'
+import * as postcss from 'postcss'
+import * as syntax from 'postcss-scss'
+import dumbAnalyzer from './analyzers/dumbAnalyzer'
 
-const syntax = require('postcss-scss');
-const multiGlob = require('multi-glob');
-
-const readFile = promisify(fs.readFile);
-const glob = promisify(multiGlob.glob);
-
-function usage() {
-  return `Usage: csslusterfuck /path/to/style.scss`;
+function parseSingleSource(
+  source: string,
+  filename: string
+): Promise<postcss.Node> {
+  return postcss([])
+    .process(source, {
+      from: filename,
+      syntax,
+    })
+    .then(result => result.root)
 }
 
-//
-// AST helpers
-interface ASTWalker {
-  (node: postcss.Node): void;
+interface CsslusterfuckOptions {
+  filenames?: string[]
+  unnormalized?: boolean
+  mode: 'dumb'
 }
 
-function walkAST(fn: ASTWalker, node: postcss.Node): void {
-  fn(node);
-  switch (node.type) {
-    case 'root':
-    case 'rule':
-    case 'atrule':
-      if (node.nodes)
-        node.nodes.forEach(child => walkAST(fn, child));
-      // Intentional fall-through
-    case 'decl':
-    case 'comment':
-  }
-}
-
-interface ASTFilter extends ASTWalker {
-  (node: postcss.Node): boolean;
-}
-
-function filterAST(fn: ASTFilter, node: postcss.Node) {
-  const nodes: postcss.Node[] = [];
-
-  walkAST(_filterAST, node);
-
-  return nodes;
-
-  function _filterAST(node: postcss.Node) {
-    if (fn(node))
-      nodes.push(node);
-  }
-}
-
-//
-// Analyzation matrix
-function isDeclaration(node: postcss.Node): node is postcss.Declaration {
-  return node.type === 'decl';
-}
-
-interface MatrixRow {
-  [key: string]: number;
-}
-
-interface Matrix {
-  [key: string]: MatrixRow;
-}
-
-function _createMatrix(rules: postcss.Rule[]): Matrix {
-  const matrix: Matrix = {};
-
-  rules.forEach(rule => {
-    if (!rule.nodes)
-      return;
-
-    const decls = rule.nodes.filter(isDeclaration) as postcss.Declaration[];
-    for(let i = 0, l = decls.length; i < l; i++) {
-      for(let j = i + 1, l = decls.length; j < l; j++) {
-        // NOTE: Do I really need to normalize the matrix? Doesn't
-        // absolute count actually be valuable information?
-        const decl1 = decls[i];
-        const decl2 = decls[j];
-
-        if(!matrix[decl1.prop])
-          matrix[decl1.prop] = {};
-        matrix[decl1.prop][decl2.prop] = (matrix[decl1.prop][decl2.prop] || 0) + 1;
-
-        if(!matrix[decl2.prop])
-          matrix[decl2.prop] = {};
-        matrix[decl2.prop][decl1.prop] = (matrix[decl2.prop][decl1.prop] || 0) + 1;
-      }
-    }
-  });
-
-  return matrix;
-}
-
-type VectorWeight = {
-  vector: number[];
-  rule: postcss.Rule;
-  weight: number;
-  filename: string;
-}
-
-type RuleWithFilename = [string, postcss.Rule];
-
-function analyzeRules(rules: RuleWithFilename[]): VectorWeight[] {
-  const matrix = _createMatrix(map(rules, ([_, rule]) => rule));
-  // Not sure if I even need to sort these..
-  // const sorted = _sortMatrix(matrix);
-  // ...Just go over the rules again, check it's values against the matrix
-  // Collect decl vectors from rule, for instance [decl1, decl2, decl3]
-  // Create index vectors from different permuations to get [[prop1 prop2] [prop1 prop3] [prop2 prop3]]
-  // Map those into a vector of [value1 value2 value3] and compute vector length
-  // Ok this is now implemented, but what then?
-  // Now we would have the enhance the decl vector by trying different permutations
-  // And we would have to test the rules against other rules to show what are the
-  // similar blocks, showing those rules with the set of properties used
-  const vectors = rules.map(([filename, rule]) => {
-    if (!rule.nodes)
-      return;
-
-    const decls = rule.nodes.filter(isDeclaration) as postcss.Declaration[];
-    if (decls.length < 2)
-      return;
-
-    const vector = [];
-    for(let i = 0, l = decls.length; i < l; i++) {
-      for(let j = i + 1, l = decls.length; j < l; j++) {
-        const decl1 = decls[i];
-        const decl2 = decls[j];
-        vector.push(matrix[decl1.prop][decl2.prop]);
-      }
-    }
-
-    return {vector, rule, filename};
+async function csslusterfuck(sources: string[], options: CsslusterfuckOptions) {
+  const _options = defaults({}, options, {
+    filenames: sources.map((_, n) => `<source #${n}>`),
+    unnormalized: false,
   })
+  const asts = await Promise.all<postcss.Node>(
+    sources.map((source, n) => parseSingleSource(source, _options.filenames[n]))
+  )
 
-  // We should connect these to the relevant css rule
-  const len = rules.length;
-  const weights = vectors
-    .filter(rules => !!rules)
-    // Normalize vectors
-    .map(vector => ({
-      ...vector,
-      weight: Math.sqrt(vector!.vector.reduce((a, b) => a + b * b, 0)) / len,
-    } as VectorWeight));
+  if (options.mode === 'dumb') return dumbAnalyzer(asts, _options)
 
-  const sorted = sortBy(weights, (vector: VectorWeight) => -vector.weight);
-  return sorted;
+  return null
 }
 
-//
-// Parse AST and collect Rules
-function isRule(node: postcss.Node): node is postcss.Rule {
-  return node.type === 'rule';
-}
-
-function getLineNumber(rule: postcss.Rule) {
-  // return `${rule.source.start!.line}:${rule.source.start!.column}-${rule.source.end!.line}:${rule.source.end!.column}`;
-  return `${rule.source.start!.line}:${rule.source.start!.column}`;
-}
-
-
-async function readRules(filename: string): Promise<RuleWithFilename[]> {
-  const data = await readFile(filename);
-  const result = await postcss([]).process(data, {syntax});
-  const rules = filterAST(isRule, result.root as postcss.Node) as postcss.Rule[];
-  return rules.map(rule => [filename, rule] as RuleWithFilename);
-}
-
-//
-// Entry point
-async function csslusterfuck(filenames: string[]) {
-  const _rules = await Promise.all(filenames.map(readRules)) as [[[string, postcss.Rule]]];
-  const rules = flatMap(_rules);
-  // console.log('Rules', ...rules);
-  const analysis = analyzeRules(rules);
-
-  if (analysis.length > 0)
-    console.log('I might have found some obnoxious CSS rules:\n');
-
-  analysis.slice(0, 10).forEach((vector) => {
-    const rule: postcss.Rule = vector.rule;
-    console.log(`${vector.filename}#${getLineNumber(rule)} (${vector.weight})`);
-    // TODO: Eliminate rule children
-    console.log(rule.toString());
-  });
-}
-
-if (process.argv.length < 3) {
-  console.log(usage());
-  process.exit();
-}
-
-glob(process.argv[2])
-  .then(csslusterfuck)
-  .catch((err: Error) => console.log(err.message, err));
+export default csslusterfuck
